8dd76db07b19f8589f0a7666477742d59b921c60
Richard Randell <richrdrandell2@outlook.com>
2021-04-26 13:54:47 -0230

    updated

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,20 @@
-console.log('Express Tutorial')
+const express = require('express')
+const app = express()
+const {products} = require('./data')
+
+
+app.get('/', (req,res)=>{
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+})
+app.get('/api/products', (req, res)=>{
+    const newProducts = products.map((product)=>{
+        const {id, name, image}= product
+        return {id, name, image}
+    })
+    res.json(newProducts)
+})
+
+
+app.listen(5000, ()=>{
+    console.log('Server is listening on port 5000...')
+})
